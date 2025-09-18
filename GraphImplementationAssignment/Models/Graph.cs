b3c3fc10cff95f82580ae4fa07009cbf8d3564ab
Vinicius Gobbi <vgobbi.8@gmail.com>
2025-09-18 20:34:04 -0300

    First Commit

---
+++ GraphImplementationAssignment/Models/Graph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphImplementationAssignment.Models
+{
+    public class Graph
+    {
+        public bool Directed { get; set; }
+        public List<Vertex> Vertices { get; set; }
+        public List<Edge> Edges { get; set; }
+    }
+}
